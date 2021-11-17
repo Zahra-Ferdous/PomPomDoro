36a32e1fb2458984ca6d39304d383075cf3f77b3
Zahra <antora0060@gmail.com>
2021-11-17 21:32:11 +0600

    new java class Zahra created

---
+++ app/src/main/java/com/example/pomodoro/Zahra.java
@@ -0,0 +1,18 @@
+package com.example.pomodoro;
+
+public class Zahra {
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public Zahra(int a) {
+        this.a = a;
+    }
+
+    int a;
+
+}
